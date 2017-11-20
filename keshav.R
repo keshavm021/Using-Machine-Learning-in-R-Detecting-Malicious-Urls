f8a4221ea41ab7c77b0bf7c7709f3cd53556eb2a
keshavm021 <keshavm021@gmail.com>
2017-11-21 01:06:58 +0530

    Update keshav.R

--- keshav.R
+++ keshav.R
@@ -1,5 +1,12 @@
 library(glmnet)
 library(dplyr)
+library(rpart)
+library(e1071)
+library(rms)
+library(ggplot2)
+library(lattice)
+library(survival)
+
 getTokens <- function(input)
 {
   tokensbyslash = strsplit(input,"/")
@@ -30,9 +37,8 @@ Tl <- function()
                          {
                            corpus=d[1]
                          }
-                         vectorizer =
-                           ##vectorizer = TfidfVectorizer(tokenizer=getTokens)
-                           ##X = vectorizer.fit_transform(corpus)
+                           vectorizer = TfidfVectorizer(tokenizer=getTokens)
+                           X = vectorizer.fit_transform(corpus)
                            n_sample =NROW(X)
                          w=0.8
                          X_train = X[0:(w*n_sample),]
@@ -40,14 +46,45 @@ Tl <- function()
                          X_test = X[((w*n_sample)+1):n_sample,]
                          y_test = y[((w*n_sample)+1):n_sample]
 set.seed(0)
+##1.Logistic Regression
 model_lambda <- cv.glmnet(as.matrix(X_train), as.factor(y_train),
                           nfolds = 10, alpha=1, family="binomial", type.measure="class")
-best_s  <- model_lambda$lambda.1se
 pred <- as.numeric(predict(model_lambda, newx=as.matrix(X_test), type="class" , s=best_s))
-print(best_s)
-print(sum(y_test==pred)/NROW(pred))
+Lg_sum=(y_test==pred)/NROW(pred)
+print("Logistic Regression prediction score:",Lg_sum)
+##2.Linear Regression
+x <- cbind(X_train,y_train)
+linear <- lm(y_train ~ ., data=x)
+summary(linear)
+#Output for linear regression is:
+ln_predict = predict(linear,X_test)
+print("Linear Regression Prediction score is:",ln_predict)
+##3.Decision Tree
+fit <- rpart(y_train ~ ., data = x,method="class")
+summary(fit)
+DT_predict = predict(fit,X_test)
+print("Decision Tree Prediction Score:",DT_predict)
+##4.SVM(Support Vector Machine )
+fit1 <-svm(y_train ~ ., data = x)
+summary(fit1)
+Svm_prediction = predict(fit1,X_test)
+print("SVM prediction score is:",Svm_prediction)
+##5.Naive Bayes
+fit2 <-naiveBayes(y_train ~ ., data = x)
+summarise(fit2)
+Naive_pred = predict(fit2,X_test)
+print("Naive bayes Prediction Score is :",Naive_pred)
+##6.KNN(K-Nearest Neighbours)
+fit3 <-knn(y_train ~ ., data = x,k=5)
+summary(fit3)
+KNN_pred= predict(fit3,X_test)
+print("KNN Score is:",KNN_pred)
+##NOW plotting a Graph for all the prediction
+p <- rbind(Lg_sum,ln_predict,DT_predict,Svm_prediction,Naive_pred,KNN_pred)
+plot(p,varypred=TRUE,adj,subtitle=FALSE)
+
+
 }  
+Tl()
 
-
-  
 
